2a8dae4743d7b0f2b5390c6276fd1b5d493cd7ab
Lidiya Dzhumayeva <81679574+lidiyacommercetools@users.noreply.github.com>
2023-11-15 15:33:32 -0500

    Create image_converter_medium.py

---
+++ image_converter_medium.py
@@ -0,0 +1,12 @@
+directory = '/Users/lidiyadzhumayeva/Desktop/goodstore_images'
+files = os.listdir(directory)
+index = 0
+while index < len(files):
+    filename = files[index]
+    if filename.endswith('.jpeg'):
+        with open(os.path.join(directory, filename)) as f:
+            image = Image.open(filename)
+            new_image = image.resize((400, 400))
+            new_image.save("updated_images/" + str(filename)[:-5] + "-medium.jpeg", "jpeg")
+    index += 1
+    
