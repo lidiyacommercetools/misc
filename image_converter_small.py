ab1e02975962f649eccb1651a5c3cbebe7f77691
Lidiya Dzhumayeva <81679574+lidiyacommercetools@users.noreply.github.com>
2024-11-13 08:47:42 -0500

    Update image_converter_small.py

--- image_converter_small.py
+++ image_converter_small.py
@@ -9,6 +9,6 @@ while index < len(files):
   if filename.endswith('.jpeg'):
     with open(os.path.join(directory, filename)) as f:
       image = Image.open(filename)
-      new_image = image.resize((150, 150))
-      new_image.save("updated_images/" + str(filename)[:-5] + "-small.jpeg", "jpeg")
+      image.thumbnail((150,150))
+      image.save("updated_images/" + str(filename)[:-5] + "-small.jpeg", "jpeg")
   index += 1
