2b4bf2ac5c552d4198843c4c50fad31d1b1765b7
Lidiya Dzhumayeva <81679574+lidiyacommercetools@users.noreply.github.com>
2024-11-13 08:47:02 -0500

    Update image_converter_medium.py

--- image_converter_medium.py
+++ image_converter_medium.py
@@ -9,7 +9,7 @@ while index < len(files):
     if filename.endswith('.jpeg'):
         with open(os.path.join(directory, filename)) as f:
             image = Image.open(filename)
-            new_image = image.resize((400, 400))
-            new_image.save("updated_images/" + str(filename)[:-5] + "-medium.jpeg", "jpeg")
+            image.thumbnail((400,400))
+            image.save("updated_images/" + str(filename)[:-5] + "-medium.jpeg", "jpeg")
     index += 1
     
